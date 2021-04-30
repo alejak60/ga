21b6ebe957df2ecd19d3ac17349aa078ce723d22
8766853 <alejak60@gmail.com>
2021-04-30 14:53:08 -0500

    first commit

---
+++ src/test/java/com/demoaut/newtours/constants/IConstants.java
@@ -0,0 +1,9 @@
+package com.demoaut.newtours.constants;
+
+public interface IConstants {
+
+	public static final String CHROMEDRIVERPATH = "test-artifacts//Drivers//Mac//chromedriver";
+	public static final int IMPLICITWAITSTND=30;
+	public static final String CHROME = "CHROME";
+	
+}
